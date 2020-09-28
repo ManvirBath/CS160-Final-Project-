9ae12c4388d63e84a499f9b1524fc22662e1dd57
Ralph <ralphstevendc@gmail.com>
2020-09-27 18:05:22 -0700

    started models

--- Full_Project_Folder/test/models.py
+++ Full_Project_Folder/test/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+""" ROLES """
+class Manager(models.Model):
+
+class Customer(models.Model):
+
+""" ACCOUNTS """
+class SavingsAccount(models.Model):
+
+class CheckingAccount(models.Model):
+
+class DebitCard(models.Model):
+
+class Transactions(models.Model):
+
+class CustomerSavings(models.Model):
+
+class CustomerChecking(models.Model):
+
+class CheckingDebit(models.Model):
+
+""" RECEIVED TRANSACTIONS """
+class SavingsTransaction(models.Model):
+
+class CheckingTransaction(models.Model):
+
+class CardTransaction(models.Model):
+
