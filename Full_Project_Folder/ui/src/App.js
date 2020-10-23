f40a7d4449f76befc6deb0879d505d458978214c
aniquazad <aniqua.azad@gmail.com>
2020-10-22 17:11:39 -0700

    created transfer pages, fixed navbar to be own component, altered user dashboard

--- Full_Project_Folder/ui/src/App.js
+++ Full_Project_Folder/ui/src/App.js
@@ -1,59 +1,81 @@
-import React from 'react';
-import './App.css';
-import Login from './components/Login';
-import Recovery from './components/Recovery';
-import Register from './components/Register';
-import GMap from './components/GMap';
-import UserDashboard from './components/UserDashboard';
-import DepositCheck from './components/DepositCheck';
-import DepositCheckConfirm from './components/DepositCheck/DepositCheckConfirm';
-import DepositCheckTransaction from './components/DepositCheck/DepositCheckTransaction';
+import React from "react";
+import "./App.css";
+import Login from "./components/Login";
+import Recovery from "./components/Recovery";
+import Register from "./components/Register";
+import GMap from "./components/GMap";
+import UserDashboard from "./components/UserDashboard";
+import DepositCheck from "./components/DepositCheck";
+import DepositCheckConfirm from "./components/DepositCheck/DepositCheckConfirm";
+import DepositCheckTransaction from "./components/DepositCheck/DepositCheckTransaction";
+import TransferInternal from "./components/Transfer/TransferInternal";
+import TransferInternalConfirm from "./components/Transfer/TransferInternalConfirm";
+import TransferInternalTransaction from "./components/Transfer/TransferInternalTransaction";
+import TransferExternal from "./components/Transfer/TransferExternal";
+import TransferExternalConfirm from "./components/Transfer/TransferExternalConfirm";
+import TransferExternalTransaction from "./components/Transfer/TransferExternalTransaction";
+
 import {
-    BrowserRouter as Router,
-    Route,
-    Switch,
-    Redirect,
-} from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { dark: false };
-    }
-    render() {
-        const { dark } = this.state;
-        return (
-            <div className={`App ${dark ? 'dark' : ''}`}>
-                <Router>
-                    <Switch>
-                        <Route
-                            exact
-                            path="/login"
-                            render={(props) => <Login {...props} dark={dark} />}
-                        />
-                        <Route path="/register" component={Register} />
-                        <Route path="/recovery" component={Recovery} />
-                        <Route path="/gmap" component={GMap} />
-                        <Route
-                            path="/userdashboard"
-                            component={UserDashboard}
-                        />
-                        <Route path="/depositcheck" component={DepositCheck} />
-                        <Route
-                            path="/depositcheckconfirm"
-                            component={DepositCheckConfirm}
-                        />
-                        <Route
-                            path="/depositchecktransaction"
-                            component={DepositCheckTransaction}
-                        />
-                        <Redirect from="*" to="/login" />
-                    </Switch>
-                </Router>
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = { dark: false };
+  }
+  render() {
+    const { dark } = this.state;
+    return (
+      <div className={`App ${dark ? "dark" : ""}`}>
+        <Router>
+          <Switch>
+            <Route
+              exact
+              path="/login"
+              render={(props) => <Login {...props} dark={dark} />}
+            />
+            <Route path="/register" component={Register} />
+            <Route path="/recovery" component={Recovery} />
+            <Route path="/gmap" component={GMap} />
+            <Route path="/userdashboard" component={UserDashboard} />
+            <Route path="/depositcheck" component={DepositCheck} />
+            <Route
+              path="/depositcheckconfirm"
+              component={DepositCheckConfirm}
+            />
+            <Route
+              path="/depositchecktransaction"
+              component={DepositCheckTransaction}
+            />
+            <Route path="/transferinternal" component={TransferInternal} />
+            <Route
+              path="/transferinternalconfirm"
+              component={TransferInternalConfirm}
+            />
+            <Route
+              path="/transferinternaltransaction"
+              component={TransferInternalTransaction}
+            />
+            <Route path="/transferexternal" component={TransferExternal} />
+            <Route
+              path="/transferexternalconfirm"
+              component={TransferExternalConfirm}
+            />
+            <Route
+              path="/transferexternaltransaction"
+              component={TransferExternalTransaction}
+            />
+            <Redirect from="*" to="/login" />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
--- Full_Project_Folder/ui/src/components/DepositCheck/DepositCheckConfirm.jsx
+++ Full_Project_Folder/ui/src/components/DepositCheck/DepositCheckConfirm.jsx
@@ -1,39 +1,55 @@
-import React from 'react';
-import './DepositCheck.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./DepositCheck.css";
+import { Link } from "react-router-dom";
 
 class DepositCheckConfirm extends React.Component {
-    render() {
-        return (
-            <div className="DepositCheckConfirm">
-                <h1 className="PageHeader" class="jumbotron">
-                    Confirm check deposit
-                </h1>
-                <h6 className="PageHeader2" class="lead">
-                    Please confirm your details before submitting.
-                </h6>
-                <div className="details">
-                    <h4>Deposit to: [insert account info]</h4>
-                    <h4>Amount: [insert amount]</h4>
-                    <h4>Memo: [insert memo]</h4>
-                    <h4>Check file: [insert check image info]</h4>
-                    <h4>[insert check image]</h4>
-                </div>
-                <div className="buttons">
-                    <Link to="/depositcheck">
-                        <button type="button" class="btn btn-warning">
-                            Make Changes
-                        </button>
-                    </Link>
-                    <Link to="/depositchecktransaction">
-                        <button type="button" class="btn btn-primary">
-                            Submit
-                        </button>
-                    </Link>
-                </div>
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="DepositCheckConfirm">
+        <h1 className="PageHeader" class="jumbotron">
+          Confirm check deposit
+        </h1>
+        <h6 className="PageHeader2" class="lead">
+          Please confirm your details before submitting.
+        </h6>
+        <div className="details">
+          <h4>
+            Deposit to: [insert account info]{this.props.location.account}
+          </h4>
+          <h4>Amount: {this.props.location.amount}</h4>
+          <h4>Memo: {this.props.location.memo}</h4>
+          <h4>Check file: {this.props.location.check_image}</h4>
+          <img src={this.props.location.check_image}></img>
+        </div>
+        <div className="buttons">
+          <Link to="/depositcheck">
+            <button type="button" class="btn btn-danger">
+              Don't make deposit
+            </button>
+          </Link>
+          <Link
+            to={{
+              pathname: "/depositchecktransaction",
+              account: this.props.location.account,
+              amount: this.props.location.amount,
+              memo: this.props.location.memo,
+              file: this.props.location.file,
+              check_image: this.props.location.check_image,
+            }}
+          >
+            <button type="button" class="btn btn-primary">
+              Submit
+            </button>
+          </Link>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default DepositCheckConfirm;
