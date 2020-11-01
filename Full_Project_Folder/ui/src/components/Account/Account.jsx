2cc4498b03c49ff667f2908992dc8a4946c7cd72
Manvir Bath <manvir.bath@sjsu.edu>
2020-11-01 00:23:38 -0700

    Added accounts page and more css modifications

--- Full_Project_Folder/ui/src/components/Account/Account.jsx
+++ Full_Project_Folder/ui/src/components/Account/Account.jsx
@@ -10,106 +10,86 @@ class Account extends React.Component {
         super(props);
 
         this.state = {
-            savings: '',
             transaction_number: '',
             amount: '',
             date: '',
+            trans_type: '',
+            check_path: '',
             location: '',
+            memo: '',
         };
 
-        this.state = {
-            savings: {
-                1244221: [
-                    {
-                        transaction_number: '121212',
-                        amount: '$500.00',
-                        date: '01/14/2020',
-                        location: 'ATM Berryessa',
-                    },
-                ],
+        let savings = {
+            1244221: [
+                {
+                    transaction_number: '121212',
+                    amount: '500.00',
+                    date: '2020-10-31',
+                    trans_type: 'Deposit',
+                    check_path: 'C:fakepath\fakecheck.img',
+                    location: 'Online',
+                    memo: '1st paycheck',
+                },
+            ],
 
-                5355228: [
-                    {
-                        transaction_number: '634342',
-                        amount: '$200.00',
-                        date: '23/12/2020',
-                        location: 'ATM N. Jackson',
-                    },
-                ],
-            },
+            5355228: [
+                {
+                    transaction_number: '634342',
+                    amount: '200.00',
+                    date: '2020-17-03',
+                    trans_type: 'Withdraw',
+                    check_path: '',
+                    location: 'ATM',
+                    memo: '',
+                },
+            ],
         };
     }
 
-    savings(e) {}
     transaction_number(e) {}
     amount(e) {}
     date(e) {}
+    check_path(e) {}
+    trans_type(e) {}
     location(e) {}
+    memo(e) {}
 
     render() {
-        const {
-            savings,
-            transaction_number,
-            amount,
-            date,
-            location,
-        } = this.state;
         return (
-            <div className="account">
+            <div className="Account">
                 <UserNavigationBar />
-                <div className="container">
-                    <h1 id="saving">Savings 1234 $100.00"</h1>
-                    <h2 id="title">Transactions</h2>
-                    <table>
-                        <thead>
-                            <th>
-                                <td>transaction_number</td>
-                                <td>amount</td>
-                                <td>date</td>
-                                <td>location</td>
-                            </th>
-                        </thead>
-                        <tbody>
-                            {Object.keys(this.state.savings).map(
-                                (transaction_number, key) => {
-                                    return (
-                                        <tr key={key}>
-                                            {this.state.savings[
-                                                transaction_number
-                                            ].map((transaction, index) => {
-                                                return (
-                                                    <tr
-                                                        key={
-                                                            String(key) +
-                                                            String(index)
-                                                        }
-                                                    >
-                                                        <td>
-                                                            {
-                                                                transaction.transaction_number
-                                                            }
-                                                        </td>
-                                                        <td>
-                                                            {transaction.amount}
-                                                        </td>
-                                                        <td>
-                                                            {transaction.date}
-                                                        </td>
-                                                        <td>
-                                                            {
-                                                                transaction.location
-                                                            }
-                                                        </td>
-                                                    </tr>
-                                                );
-                                            })}
-                                        </tr>
-                                    );
-                                }
-                            )}
-                        </tbody>
-                    </table>
-                </div>
+                <h1 className="acctTypeName">Savings 1234</h1>
+                <h4 className="acctBalance">Account Balance: $100.00</h4>
+                <h2 id="title">Transactions</h2>
+                <table>
+                    <tr>
+                        <th>Transaction Number</th>
+                        <th>Date</th>
+                        <th>Amount</th>
+                        <th>Type</th>
+                        <th>Memo</th>
+                        <th>Location</th>
+                        <th>Check Path</th>
+                    </tr>
+                    <tr>
+                        <td>121212</td>
+                        <td>2020-10-31</td>
+                        <td>500.00</td>
+                        <td>Deposit</td>
+                        <td>1st paycheck</td>
+                        <td>Online</td>
+                        <td>C:fakepath\fakecheck.img</td>
+                    </tr>
+                    <tr>
+                        <td>634342</td>
+                        <td>2020-17-03</td>
+                        <td>200.00</td>
+                        <td>Withdraw</td>
+                        <td></td>
+                        <td>ATM</td>
+                        <td></td>
+                    </tr>
+                </table>
             </div>
         );
     }
