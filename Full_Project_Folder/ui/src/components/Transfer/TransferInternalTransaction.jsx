79dd650ffd7b8adabd2d9d7cdc9cd57f19f43c4e
aniquazad <aniqua.azad@gmail.com>
2020-11-13 09:35:01 -0800

    merge from master

--- Full_Project_Folder/ui/src/components/Transfer/TransferInternalTransaction.jsx
+++ Full_Project_Folder/ui/src/components/Transfer/TransferInternalTransaction.jsx
@@ -1,87 +1,95 @@
-import React from "react";
-import "./Transfer.css";
-import { Link } from "react-router-dom";
-import UserNavigationBar from "../UserNavBar/UserNavBar";
+import React from 'react';
+import './Transfer.css';
+import { Link } from 'react-router-dom';
+import UserNavigationBar from '../UserNavBar/UserNavBar';
 
 class TransferInternalTransaction extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      status_response:
-        "Thank you for your transfer request! Please contact us if you have any questions or concerns.",
-      alert_type: "alert alert-success",
-      to_acct: this.props.location.to_acct || localStorage.getItem('to_acct'),
-      from_acct: this.props.location.from_acct || localStorage.getItem('from_acct'),
-      amount: this.props.location.amount || localStorage.getItem('amount'),
-      memo: this.props.location.memo || localStorage.getItem('memo'),
-      
+    constructor(props) {
+        super(props);
+        this.state = {
+            status_response: this.props.location.status_response,
+            alert_type: this.props.location.alert_type,
+            to_acct:
+                this.props.location.to_acct || localStorage.getItem('to_acct'),
+            from_acct:
+                this.props.location.from_acct ||
+                localStorage.getItem('from_acct'),
+            amount:
+                this.props.location.amount || localStorage.getItem('amount'),
+            memo: this.props.location.memo || localStorage.getItem('memo'),
+        };
+    }
+
+    componentDidMount() {
+        console.log(this.state.from_acct);
+        console.log(this.state.to_acct);
+        console.log(this.state.amount);
+        console.log(this.state.memo);
+        console.log(this.props.location.alert_type);
+        // window.addEventListener('popstate', (event) => {
+        //   if (event.state) {
+        //     alert("Please press 'Go to Dashboard' button")
+        //   }
+        //  }, false);
+
+        //  window.addEventListener("beforeunload", this._confirm);
+        window.history.pushState(null, '', window.location.href);
+        window.onpopstate = this._backConfirm;
+    }
+    componentWillUnmount() {
+        window.removeEventListener('beforeunload', this._confirm);
+        window.onpopstate = () => {};
+    }
+
+    _backConfirm = async () => {
+        let event = window.confirm(
+            'Cannot go back to transfer submission page. '
+        );
+        if (event) {
+            window.history.pushState(null, '', window.location.href);
+        }
     };
-  }
 
-  componentDidMount() {
-    console.log(this.state.from_acct);
-    console.log(this.state.to_acct);
-    console.log(this.state.amount);
-    console.log(this.state.memo);
+    _confirm = (e) => {
+        var confirmationMessage = 'o/';
+        e.returnValue = confirmationMessage;
+        return confirmationMessage;
+    };
 
-    // window.addEventListener('popstate', (event) => {
-    //   if (event.state) {
-    //     alert("Please press 'Go to Dashboard' button")
-    //   }
-    //  }, false);
-
-    //  window.addEventListener("beforeunload", this._confirm);
-     window.history.pushState(null, "", window.location.href);
-     window.onpopstate = this._backConfirm;
-  }
-  componentWillUnmount() {
-      window.removeEventListener("beforeunload", this._confirm);
-      window.onpopstate = () => { }
-  }
-
-  _backConfirm = async () => {
-      let event = window.confirm("Cannot go back to transfer submission page. ");
-      if(event){
-          window.history.pushState(null, "", window.location.href);
-      }
-  }
-
-  _confirm = (e) => {
-      var confirmationMessage = "\o/";
-      e.returnValue = confirmationMessage;
-      return confirmationMessage;
-  }
-  
-  render() {
-    return (
-      <div className="DepositCheckTransaction">
-        <UserNavigationBar active={2} />
-        <div className="internal-transaction-headerDivTransaction">
-          <div
-            className="TransactionAlert"
-            class={this.state.alert_type}
-            role="alert"
-            id="depositchecktransaction-ty"
-          >
-            <p>{this.state.status_response}</p>
-          </div>
-        </div>
-        <div className="transfer-internal-transactionInfo">
-          <h4>From: {this.state.from_acct}</h4>
-          <h4>To: {this.state.to_acct}</h4>
-          <h4>Amount: {this.state.amount}</h4>
-          <h4>Memo: {this.state.memo}</h4>
-        </div>
-        <div className="transfer-internal-transactionbuttons">
-        <Link to="/userdashboard">
-            <button type="submit" class="btn btn-primary" id="internal-transaction-backto-dash">
-              Back to Dashboard
-            </button>
-        </Link>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="DepositCheckTransaction">
+                <UserNavigationBar active={2} />
+                <div className="internal-transaction-headerDivTransaction">
+                    <div
+                        className="TransactionAlert"
+                        class={this.state.alert_type}
+                        role="alert"
+                        id="depositchecktransaction-ty"
+                    >
+                        <p>{this.state.status_response}</p>
+                    </div>
+                </div>
+                <div className="transfer-internal-transactionInfo">
+                    <h4>From: {this.state.from_acct}</h4>
+                    <h4>To: {this.state.to_acct}</h4>
+                    <h4>Amount: {this.state.amount}</h4>
+                    <h4>Memo: {this.state.memo}</h4>
+                </div>
+                <div className="transfer-internal-transactionbuttons">
+                    <Link to="/userdashboard">
+                        <button
+                            type="submit"
+                            class="btn btn-primary"
+                            id="internal-transaction-backto-dash"
+                        >
+                            Back to Dashboard
+                        </button>
+                    </Link>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default TransferInternalTransaction;
