51725b0930ee8486024fe02bce5508556905377c
Manvir Bath <manvir.bath@sjsu.edu>
2020-11-13 20:09:33 -0800

    more design changes

---
+++ Full_Project_Folder/ui/src/components/Contact/Contact.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import './Contact.css';
+import Logo from '../Logo';
+import { Jumbotron, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+class Contact extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div className="contact">
+                <Logo color="rgb(255,255,255)" text="Deep Learning Bank"></Logo>
+                <div id="contactus-greeting">Contact Us</div>
+                <Jumbotron className="contact-jumbotron">
+                    <div className="contact-upper">
+                        <div id="line1">
+                            Have questions? Need to make changes to your profile
+                            or account information?
+                        </div>{' '}
+                        <div id="line2">
+                            If you need any assistance at all, feel free to
+                            contact us.
+                        </div>
+                        <div id="line3">24/7, 365 Customer Service.</div>
+                    </div>
+                    <span className="contact-office-location">
+                        <div id="office-location-header">Office Location: </div>
+                        <div className="office-location">
+                            123 Avenue X
+                            <div id="office-location2">
+                                Gotham City, New Jersey{' '}
+                            </div>
+                            <div id="office-location3">07863 </div>
+                            <div id="office-location4">USA </div>
+                        </div>
+                    </span>
+                    <span className="contact-phone">
+                        <div id="contact-number-header">Phone:</div>
+                        <div id="contact-number">(732) 583-3950 </div>
+                    </span>
+                    <span className="contact-email">
+                        <div id="contact-email-header">Email:</div>
+                        <div id="contact-email">DeepLearningBank@email.com</div>
+                    </span>
+                </Jumbotron>
+                <div className="footer">
+                    <Link to="/main">Home page</Link>
+                </div>
+                <div className="footer">
+                    <Link to="/userdashboard">Dashboard</Link>
+                </div>
+            </div>
+        );
+    }
+}
+export default Contact;
